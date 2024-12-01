4c578b9a91c485f8470d93a40bcbc25951aaa61d
TreeFall1 <andreevdaniil06an@gmail.com>
2024-12-01 14:58:22 +0300

    Update

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import "./globals.css";
 
-
 export const metadata = {
   title: "Я.Электрички",
   description: "Покупка билетов на электрички",
@@ -9,9 +8,19 @@ export const metadata = {
   },
 };
 
+export const viewport = {
+  initialScale: 1,
+  width: 'device-width',
+	themeColor: '#333333',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
+			<head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
 				{children}
 			</body>
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,47 @@
 /** @type {import('next').NextConfig} */
 import path from 'path';
+import withPWA from 'next-pwa';
 
-const nextConfig = {
-
-};
+const nextConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+	disable: process.env.NODE_ENV === "development",
+	runtimeCaching: [
+    {
+      urlPattern: /\.(?:js|css|html)$/, // Кэширование статических файлов
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'static-resources',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 дней
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|gif|svg)$/, // Кэширование изображений
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images-cache',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        },
+      },
+    },
+    {
+      urlPattern: /.*/,
+      handler: 'NetworkFirst', // или 'CacheFirst'
+      options: {
+        cacheName: 'my-cache',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        },
+      },
+    },
+  ],
+});
 
 export default nextConfig;
